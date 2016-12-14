aa8ecb79158a9a86c1e5a6bcdacc3feb730d9168
Bowen Zhong <bz4e14@soton.ac.uk>
2016-12-14 15:09:14 +0800

    Comet

---
+++ script_http/18.3/chat.js
@@ -0,0 +1,61 @@
+var http = require('http');
+var clientui = require('fs').readFileSync("chatclient.html");
+var emulation = require('fs').readFileSync("EventSourceEmulation.js");
+
+var clients = [];
+setInterval(function() {
+	clients.forEach(function(client) {
+		client.write(":ping?n");
+	});
+}, 20000);
+
+var server = new http.Server();
+server.on("request", function(request, response) {
+	var url = require("url").parse(request.url);
+	if (url.pathname === '/') {
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("<script>" + emulation + "</script>");
+		response.end();
+		return;
+	} else if (url.pathname !== "/chat") {
+		response.writeHead(404);
+		response.end();
+		return;
+	}
+
+	if (request.method === "POST") {
+		request.setEncoding("utf8");
+		var body = "";
+		request.on("data", function(chunk) {
+			body += chunk;
+		});
+		request.on("end", function() {
+			response.writeHead(200);
+			response.end();
+			message = "data: " + body.replace('\n', '\ndata: ') + "\r\n\r\n";
+			clients.forEach(function(client) {
+				client.write(message);
+			});
+		});
+	} else {
+		response.writeHead(200, {"Content-Type": "text/event-stream"});
+		response.write("data: Connected\n\n");
+		request.connection.on("end", function() {
+			clients.splice(clients.indexOf(response), 1);
+			response.end();
+		});
+		clients.push(response);
+	}
+});
+
+server.listen(8000);
+
+
+
+
+
+
+
+
+
+
